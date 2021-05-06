d51cfbf49635b0ec60d22337651ddd0182a028ec
isahilgupta1306 <sg7544815@gmail.com>
2021-05-06 16:30:59 +0530

    Dark Mode Issue solved :)

--- app/src/main/java/com/example/bookbecho/MainActivity.java
+++ app/src/main/java/com/example/bookbecho/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.bookbecho;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
